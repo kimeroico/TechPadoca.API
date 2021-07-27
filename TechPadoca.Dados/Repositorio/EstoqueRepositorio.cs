60bcbf03d3e56c8cbdd13a02068eca9e2697bc5c
Kim Eroico <kim-eroico@hotmail.com>
2021-07-27 15:12:14 -0300

    Cozinha Add

---
+++ TechPadoca.Dados/Repositorio/EstoqueRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TechPadoca.Dados.Repositorio
+{
+    public class EstoqueRepositorio
+    {
+    }
+}
---
+++ TechPadoca.Dominio/Cozinha.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechPadoca.Dominio.Services;
+
+namespace TechPadoca.Dominio
+{
+    public class Cozinha : ISolicitacaoEstoque
+    {
+        public int Id { get; set; }
+        public Produto ProdutoFabricado { get; set; }
+        public int QauntidadeProduzida { get; set; }
+        public Receita Receita { get; set; }
+
+        
+        
+        void SolicitarIngredientes()
+        {
+           
+        }
+
+        void VerificarReceita()
+        {
+
+        }
+
+        void EnviarProdutoPronto()
+        {
+
+        }
+    }
+}
