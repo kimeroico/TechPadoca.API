52a6a3a537a858edf65994a511754c9ebcca7148
Kim Eroico <kim-eroico@hotmail.com>
2021-07-27 20:01:24 -0300

    Algumas correções.

--- TechPadoca.Dominio/Loja.cs
+++ TechPadoca.Dominio/Loja.cs
@@ -3,11 +3,10 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using TechPadoca.Dominio.Services;
 
 namespace TechPadoca.Dominio
 {
-    public class Loja : ISolicitacaoEstoque, ISolicitacaoCozinha
+    public class Loja
     {
         public int Id { get; set; }
         public Produto Produto { get; set; }
