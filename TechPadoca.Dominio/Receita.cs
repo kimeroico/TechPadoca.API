93f480e568fb39c5992f394e3985a8a506bb17e1
Alan <alan_pires2@hotmail.com>
2021-07-28 23:23:46 -0300

    Adicionado Receita

--- TechPadoca.Dominio/Receita.cs
+++ TechPadoca.Dominio/Receita.cs
@@ -1,12 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using TechPadoca.Dominio.Enum;
 
 namespace TechPadoca.Dominio
 {
     public class Receita
     {
+        public int Id { get; set; }
+        public Produto ProdFabricado { get; set; }
+        public Produto ProdIngrediente { get; set; }
+        public decimal QtdIngrediente { get; set; }
+        public bool Status { get; set; }
+
+        public void Cadastrar(int id, Produto prodFabricado, Produto prodIngrediente, decimal qtdIngrediente, bool status)
+        {
+            Id = id;
+            ProdFabricado = prodFabricado;
+            ProdIngrediente = prodIngrediente;
+            QtdIngrediente = qtdIngrediente;
+            Status = status;
+        }
+        public void Alterar(Produto prodFabricado, Produto prodIngrediente, decimal qtdIngrediente)
+        {
+            ProdFabricado = prodFabricado.Categoria == CategoriaDeProduto.Proprio ? prodFabricado : ProdFabricado;
+            ProdIngrediente = prodIngrediente.Categoria == CategoriaDeProduto.Ingrediente ? prodIngrediente : ProdIngrediente;
+            QtdIngrediente = qtdIngrediente;
+        }
+
+        public void Desativar()
+        {
+            Status = false;
+        }
     }
 }
