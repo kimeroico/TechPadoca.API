fa68fe16b872d3ad9744922000f6f14c4370340b
Alan <alan_pires2@hotmail.com>
2021-07-30 21:23:54 -0300

    VendaMap

---
+++ TechPadoca.Dados/Map/VendaMap.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechPadoca.Dominio;
+
+namespace TechPadoca.Dados.Map
+{
+    public class VendaMap : IEntityTypeConfiguration<Venda>
+    {
+        public void Configure(EntityTypeBuilder<Venda> builder)
+        {
+            builder.ToTable("Venda");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.ValorTotal).HasColumnType("decimal(10,2)").IsRequired();
+            builder.Property(x => x.Desconto).HasColumnType("decimal(10,2)").IsRequired();
+            builder.Property(x => x.DataDaVenda).HasColumnType("datetime").IsRequired();
+        }
+    }
+}
