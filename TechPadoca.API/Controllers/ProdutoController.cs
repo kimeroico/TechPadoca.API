27fda6337f36b8ad8cbb059c1188f47eea2d9bab
Kim Eroico <kim-eroico@hotmail.com>
2021-07-30 23:37:24 -0300

    API

---
+++ TechPadoca.API/Controllers/ProdutoController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TechPadoca.Dados.Repositorio;
+using TechPadoca.Dominio;
+
+namespace TechPadoca.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProdutoController : ControllerBase
+    {
+        private readonly ProdutoRepositorio _repo;
+
+        public ProdutoController()
+        {
+            _repo = new ProdutoRepositorio();
+        }
+
+        [HttpGet]
+        public IEnumerable<Produto> Get()
+        {
+            return _repo.SelecionarTudo();
+        }
+
+        // GET api/<ProdutoController>/5
+        [HttpGet("{id}")]
+        public Produto Get(int id)
+        {
+            return _repo.SelecionarPorId(id);
+        }
+
+        // POST api/<ProdutoController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<ProdutoController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<ProdutoController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ TechPadoca.API/DTO/ProdutoDTO.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechPadoca.API.DTO
+{
+    public class ProdutoDTO
+    {
+    }
+}
