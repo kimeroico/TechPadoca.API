afe92055bd9fe2ae14f8fb524060ebb0a89f0433
Alan <alan_pires2@hotmail.com>
2021-07-31 00:47:52 -0300

    fazendo EstoqueController

---
+++ TechPadoca.API/DTO/EstoqueDTO.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TechPadoca.API.DTO
+{
+    public class EstoqueDTO
+    {
+        public int QuantidadeTotal { get; set; }
+        public int QuantidadeMinima { get; set; }
+        public int IdProduto { get; set; }
+        public string Local { get; set; }
+    }
+}
